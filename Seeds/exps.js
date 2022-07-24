760621a63cb82c414e54d33768ee3797752ee4e6
Fabrizio Tomasi <f.tomasi.magnani@gmail.com>
2022-07-24 18:48:52 -0300

    Pequeños cambios en index.js y se crea la carpeta Seeds con experiencia laboral

---
+++ Seeds/exps.js
@@ -0,0 +1,23 @@
+const exp = new Exp(
+    {
+        donde: "Arca Ship Suppliers (Arcapieflo S.R.L.)",
+        cuando: "01/09/17 a la actualidad.",
+        haciendoQue: "Entre en Arcapieflo S.R.L. en sus comienzos como encargado del sector de cotizaciones y compras. Basicamente realizando las mismas tareas que en mi trabajo anterior. Preparando cotizaciones de listados de provisiones y materiales de los clientes, para luego realizar las ordenes de compra de la mercadería confirmada en los pedidos. En marzo, debido a la pandemia la empreza se vio obligada a reconvertirse, debido a la suspension de la temporada de cruceros, abriendo un supermercado con venta minorista. En el nuevo emprendimiento, comence a realizar las compras de mercadería para el supermercado, asi como controles de stock y mantenimiento de la base de datos de artículos. Ahora ya con la reapertura de las fronteras y los cruceros volviendo en verano, las nuevas tareas se suman a las tareas de proveeduria maritima."
+    },
+    {
+        donde: "South Atlantic Ship Supplies (Datyfyl S.A.)",
+        cuando: "02/02/09 a 28/06/2017",
+        haciendoQue: "Cotización de listados de provisiones en inglés, confección de órdenes de compra a proveedores de plaza y en tránsito. Ingreso de facturas de compra y facturación de los pedidos. Confección de documentos y gestión de trámites ante la DNA (Aprovisionamiento de buques y Reembarcos de mercadería en tránsito), Controles y ajustes de stock"
+    },
+    {
+        donde: "Harrington S.A.",
+        cuando: "22/01/07 a 01/02/09",
+        haciendoQue: "Ingreso de facturas de compra e importaciones al sistema contable, Distribución de la mercadería a los diferentes locales, Recuentos y ajustes de stock(en todas las sucursales), Cubrir licencias del encargado de depósito."
+    },
+    {
+        donde: "Macromercado Mayorista S.A. (Carrasco)",
+        cuando: "14/02/05 a 20/01/07",
+        haciendoQue: "Recuentos y ajustes de stock, Realizacion de inventarios mensuales de las secciones de venta de mercadería a granel , Confección de informes de venta, utilidad, y ajustes de stock en base a estos recuentos."
+    },
+);
+exp.save();
--- index.js
+++ index.js
@@ -13,13 +13,13 @@ db.once("open", () => {
 
 const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home")
-})
+});
 
 app.listen("3000", () => {
     console.log("Serving on port 3000");
-})
+});
