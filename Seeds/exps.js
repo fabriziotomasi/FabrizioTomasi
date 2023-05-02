a19335adf91067d8dfba1d2cadf8786be2f525e6
Fabrizio Tomasi <96629157+fabriziotomasi@users.noreply.github.com>
2023-05-02 18:04:34 +0000

    Actualizacion de la base de datos en codespaces

--- Seeds/exps.js
+++ Seeds/exps.js
@@ -13,14 +13,14 @@ const seedDB = async () => {
     const exp = new Exp(
         {
             _id: "4",
-            logo: "C:/Git/FabrizioTomasi/views/experiencia/Logos/arcapieflo.png",
+            logo: "media/imagenes/arca.jpg",
             donde: "Arca Ship Suppliers (Arcapieflo S.R.L.)",
             cuando: "01/09/17 a la actualidad.",
-            haciendoQue: "Entré en Arcapieflo S.R.L. en sus comienzos, como encargado del sector de cotizaciones y compras. Dentro de las tareas principales del cargo, se encuentran: Cotizaciones de listados de provisiones y materiales para barcos (mayoritariamente cruceros), luego una vez aprobado el pedido por el cliente, el proceso continua con la confeccion de las ordenes de compras y su envio a los proveedores correspondientes, una vez reunida la mercadería, se ingresan las facturas de compra al sistema de gestion y finalmente la emision de las facturas y documentacion de aduana correspondiente. En Diciembre de 2020, Arcapieflo S.R.L., abre en paralelo a la proveeduria marítima, un supermercado con venta minorista al publico general, donde tambien me encargo de las compras de mercadería. ."
+            haciendoQue: "Entré en Arcapieflo S.R.L. en sus comienzos, como encargado del sector de cotizaciones y compras. Dentro de las tareas principales del cargo, se encuentran: Cotizaciones de listados de provisiones y materiales para barcos (mayoritariamente cruceros), luego una vez aprobado el pedido por el cliente, el proceso continua con la confeccion de las ordenes de compras y su envio a los proveedores correspondientes, una vez reunida la mercadería, se ingresan las facturas de compra al sistema de gestion y finalmente la emision de las facturas y documentacion de aduana correspondiente. En Diciembre de 2020, Arcapieflo S.R.L., abre en paralelo a la proveeduria marítima, un supermercado con venta minorista al publico general, donde tambien me encargo de las compras de mercadería."
         },
         {
             _id: "3",
-            logo: "C:/Git/FabrizioTomasi/views/experiencia/Logos/datyfyl.png",
+            logo: "media/imagenes/sass.jpg",
             donde: "South Atlantic Ship Supplies (Datyfyl S.A.)",
             cuando: "02/02/09 a 28/06/2017",
             haciendoQue: "Cotización de listados de provisiones en inglés, confección de órdenes de compra a proveedores de plaza y en tránsito. Ingreso de facturas de compra y facturación de los pedidos. Confección de documentos y gestión de trámites ante la DNA (Aprovisionamiento de buques y Reembarcos de mercadería en tránsito), Controles y ajustes de stock"
@@ -28,14 +28,14 @@ const seedDB = async () => {
         {
             _id: "2",
             donde: "Harrington S.A.",
-            logo: "C:/Git/FabrizioTomasi/views/experiencia/Logos/harrington.png",
+            logo: "media/imagenes/harrington.png",
             cuando: "22/01/07 a 01/02/09",
             haciendoQue: "Ingreso de facturas de compra e importaciones al sistema contable, Distribución de la mercadería a los diferentes locales, Recuentos y ajustes de stock(en todas las sucursales), Cubrir licencias del encargado de depósito."
         },
         {
             _id: "1",
             donde: "Macromercado Mayorista S.A. (Carrasco)",
-            logo: "C:/Git/FabrizioTomasi/views/experiencia/Logos/macromercado.png",
+            logo: "media/imagenes/macromercado.jpg",
             cuando: "14/02/05 a 20/01/07",
             haciendoQue: "Recuentos y ajustes de stock, Realizacion de inventarios mensuales de las secciones de venta de mercadería a granel , Confección de informes de venta, utilidad, y ajustes de stock en base a estos recuentos."
         });
